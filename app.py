3ff4172d760ceebd765951d53f238395b3a60c4b
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2020-08-14 02:31:12 +0500

    check

--- app.py
+++ app.py
@@ -53,6 +53,9 @@ def registration():
       
     response = jsonify(resp)
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
     
     return response
 
